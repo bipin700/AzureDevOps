4bcc6e7d5eb6ddfbb89a48c20ec4d034ab9f3216
Bipin Thakur <ankits2107@gmail.com>
2021-10-03 23:17:16 +0530

    Fresh File Update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { SharedService } from './shared.service';
 
 import {HttpClientModule} from '@angular/common/http';
 import { FormsModule,ReactiveFormsModule } from '@angular/forms';
+import { SalesComponent } from './sales/sales.component';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import { FormsModule,ReactiveFormsModule } from '@angular/forms';
     AddEditDepComponent,
     EmployeeComponent,
     ShowEmpComponent,
-    AddEditEmpComponent
+    AddEditEmpComponent,
+    SalesComponent
   ],
   imports: [
     BrowserModule,
